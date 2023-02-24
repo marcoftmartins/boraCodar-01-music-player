4b88bb5a7ad9ebb43930c6b70360df21a22f0f29
marcoftmartins <marcofilipemartins21@gmail.com>
2023-02-24 01:00:12 +0000

    Player with music

---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center"> boraCodar #01 - Music Player </h1>
+<div align="center">
+  Week 01 challenge from #boraCodar by <a href="https://www.rocketseat.com.br/" target="_blank">Rocketseat</a>
+</div>
+</br>
+
+<p align="center">
+  <a href="#-project-features">Project features</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-technologies">Technologies</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-license">License</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+![project-preview2](https://user-images.githubusercontent.com/47821731/218233298-e6a99f83-4659-444f-934b-fc7ff64124d5.png)
+
+---
+
+## 🚧 Project features
+
+- [x] Responsive
+- [x] Play and pause music
+- [x] Progress bar
+- [x] Progressive timer
+
+## 🛠 Technologies
+
+![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+![JAVASCRIPT](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+
+## 🎨 Layout
+
+![FIGMA](https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white)
+
+You can access the original layout using [this link](https://www.figma.com/community/file/1195050524500542670).
+
+## 📋 License
+
+This project is under the IMT license.
---
+++ script.js
@@ -0,0 +1,107 @@
+const playBtn = document.querySelector('.play')
+const prevBtn = document.querySelector('.prev')
+const nextBtn = document.querySelector('.next')
+const pauseBtn = document.querySelector('.pause')
+const songTitle = document.querySelector('.song-title')
+const songArtist = document.querySelector('.artist-name')
+const songTotalTime = document.querySelector('.total-time')
+const songLastingTime = document.querySelector('.lasting-time')
+const audio = document.querySelector('#audio')
+const musicCover = document.querySelector('.music-cover')
+const player = document.querySelector('.player')
+const progressBar = document.querySelector('.track')
+
+const songs = ['Imagina','Nos2','Madruga']
+const artists = ['Frankieontheguitar ft. Ivandro, Slow J','Bispo ft. Deezy','Mike11']
+
+let songIndex = 1
+
+loadSong(songs[songIndex], artists[songIndex])
+
+function loadSong(song, artist){
+  songTitle.innerText = song
+  songArtist.innerText = artist
+  audio.src = `assets/musics/${song}.mp3`
+  musicCover.src = `assets/covers/${song}.jpg` 
+}
+
+function playSong(){
+  player.classList.add('playing')
+  playBtn.classList.add('hide')
+  pauseBtn.classList.remove('hide')
+  audio.play()
+}
+
+function pauseSong(){
+  player.classList.remove('playing')
+  playBtn.classList.remove('hide')
+  pauseBtn.classList.add('hide')
+  audio.pause()
+}
+
+function prevSong(){
+  songIndex--
+  if(songIndex<0){
+    songIndex = songs.length-1
+  }
+  loadSong(songs[songIndex],artists[songIndex])
+  playSong()
+}
+
+function nextSong(){
+  songIndex++
+  if(songIndex>songs.length-1){
+    songIndex = 0
+  }
+  loadSong(songs[songIndex],artists[songIndex])
+  playSong()
+}
+
+function updateProgess(e){
+  const {duration, currentTime} = e.srcElement
+  const progressPercentage = (currentTime/duration) * 100
+
+  progressBar.style.setProperty('--width',`${progressPercentage}%`)
+  songLastingTime.innerText = convertSecondsIntoMinutes(currentTime)
+}
+
+function setProgress(e){
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const duration = audio.duration
+
+  audio.currentTime = (clickX / width) * duration
+}
+
+function convertSecondsIntoMinutes(timeInSeconds){
+  const minutes = Math.floor(timeInSeconds/60)
+  let seconds = Math.trunc(timeInSeconds - minutes * 60)
+  let time = `0${minutes}:${seconds}`
+  if (seconds <=9)
+    time = `0${minutes}:0${seconds}`
+  return time
+}
+
+playBtn.addEventListener('click', () => {
+  const isPlaying = player.classList.contains('playing')
+
+  if(isPlaying){
+    pauseSong()
+  } else playSong()
+})
+
+pauseBtn.addEventListener('click',pauseSong)
+
+prevBtn.addEventListener('click',prevSong)
+
+nextBtn.addEventListener('click',nextSong)
+
+audio.addEventListener('timeupdate', updateProgess)
+
+audio.addEventListener('ended', nextSong)
+
+audio.onloadedmetadata = () => {
+  songTotalTime.innerText = convertSecondsIntoMinutes(audio.duration)
+}
+
+progressBar.addEventListener('click',setProgress)
